f5d698703c8fb6da219865daac3aa3c18c8ac4eb
Jeremy Donson <jjdonson@gmail.com>
2017-12-08 08:28:36 -0500

    Removed postgres py3 module from requirements.txt

--- tools/github/ghub-issues.sh
+++ tools/github/ghub-issues.sh
@@ -1,3 +1,4 @@
-ghcreateissue jeremy-donson/teladoc-exercise --title 'List issues by priority, sortably.' -useragent jeremy --body 'Every morning at 4am, I would like an email sent to me which contains an attached html file which renders project issues sortably.' --token ecaf9351d78f43859f72745c3f8db2d48936d105
-
-ghcreateissue jeremy-donson/teladoc-exercise --title '[0:0]: Prepend Issue Title with [<priority>:<chron>]' -useragent jeremy --body 'The goal is to leverage ghubz natural sort display order, etc.' --token ecaf9351d78f43859f72745c3f8db2d48936d105
+TOKEN=$(<ghub_teladoc_token)
+TITLE=''
+BODY=''
+ghcreateissue jeremy-donson/teladoc-exercise --title "${TITLE}" -useragent jeremy --body "${BODY}" --token ${TOKEN}
